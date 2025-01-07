ef31b32e13d0c8ca26fe07af531137dc81d50b04
ladavilada <bobina.vlada@gmail.com>
2025-01-06 21:35:16 -0500

    Please enter the commit message for your changes. Lines starting

--- src/main/java/algorithms/dynamic/provinces/Solution.java
+++ src/main/java/algorithms/dynamic/provinces/Solution.java
@@ -1,6 +1,15 @@
 package algorithms.dynamic.provinces;
+
 public class Solution {
 
+    /**
+     * Find total number of provinces
+     *
+     * @param isConnected You are given an n x n matrix isConnected where isConnected[i][j] = 1
+     *                    if the ith city and the jth city are directly connected,
+     *                    and isConnected[i][j] = 0 otherwise.
+     * @return the total number of provinces
+     */
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         UnionFind uf = new UnionFind(n);
@@ -18,14 +27,14 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-       int[][] isConnected = {
-        {1, 1, 0},
-        {1, 1, 0},
-        {0, 0, 1}};
+        int[][] isConnected = {
+                {1, 1, 0},
+                {1, 1, 0},
+                {0, 0, 1}};
 
-    Solution solution = new Solution();
-    int result = solution.findCircleNum(isConnected);
-    System.out.println("Number of Provinces: " + result);
+        Solution solution = new Solution();
+        int result = solution.findCircleNum(isConnected);
+        System.out.println("Number of Provinces: " + result);
     }
 
 }
--- src/main/java/algorithms/dynamic/provinces/UnionFind.java
+++ src/main/java/algorithms/dynamic/provinces/UnionFind.java
@@ -2,10 +2,19 @@ package algorithms.dynamic.provinces;
 
 public class UnionFind {
 
+    // int array to keep track of the parent node of each node
+    // helps identify connected components
     private int[] id;
+    // keep track of the size of each component
     private int[] weight;
+    // Tracks the number of disjoint sets remaining
     private int count;
 
+    /**
+     * Initialize the UnionFind data structure with n nodes
+     *
+     * @param n number of nodes
+     */
     public UnionFind(int n) {
         id = new int[n];
         weight = new int[n];
@@ -17,7 +26,17 @@ public class UnionFind {
         }
     }
 
-    public void union (int p, int q) {
+    /**
+     * Connect two nodes p and q keeping track of the size of each component(tree)
+     * <p>
+     * Uses find(x) and find(y) to determine the roots of the sets containing x and y.
+     * Uses weight to attach the smaller tree under the larger tree, optimizing the structure.
+     * If the two sets are merged, count is decremented.
+     *
+     * @param p node p
+     * @param q node q
+     */
+    public void union(int p, int q) {
         int i = find(p);
         int j = find(q);
 
@@ -31,21 +50,44 @@ public class UnionFind {
             weight[i] += weight[j];
         }
 
+        // Reduce the number of connected components
         count--;
 
     }
 
     /**
-     * Find the corresponding node root
-     * Flatten the tree up by making every other node in pass
-     * point to its grandparent.
+     * Find the corresponding root of the set containing p, traversing the id[] array structure.
+     * <p>
+     * Flatten the tree up by making every other node in pass point to its grandparent.
+     * (partial path compression)
+     * @return the corresponding root
      */
     public int find(int p) {
         while (p != id[p]) {
+            // point to grandparent
             id[p] = id[id[p]];
             p = id[p];
         }
         return p;
+    }
+
+
+    /**
+     * Find the corresponding root of the set containing p, traversing the id[] array structure.
+     * <p>
+     * Optional (full path compression) because it uses path compression to optimize the structure,
+     * making parent[x] point directly to the root for faster future lookups.
+     *
+     * @param p node p
+     * @return the corresponding root
+     */
+    public int findOpt(int p) {
+        if (id[p] != p) {
+            // pointing to the root
+            id[p] = findOpt(id[p]);
+        }
+        return id[p];
+
     }
 
     public int getCount() {
